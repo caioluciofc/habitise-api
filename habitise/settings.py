0f49718261b89e6fd7f25f56154d50dd28466faa
caioluciofc <caioluciofc@gmail.com>
2024-03-04 10:33:50 -0300

    Cors +

--- habitise/settings.py
+++ habitise/settings.py
@@ -71,10 +71,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'habitise.wsgi.application'
 AUTH_USER_MODEL = "habitise.User"
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 'https://habitise.vercel.app/'
 ]
+CSRF_TRUSTED_ORIGINS = ['https://habitise.vercel.app/']
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases

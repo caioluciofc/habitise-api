2847adcfd570a41e2d50abe4ee4416e47b49647e
caioluciofc <caioluciofc@gmail.com>
2024-03-04 10:37:19 -0300

    CSRF

--- habitise/settings.py
+++ habitise/settings.py
@@ -71,10 +71,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'habitise.wsgi.application'
 AUTH_USER_MODEL = "habitise.User"
 
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000', 'https://habitise.vercel.app/'
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000', 'https://habitise.vercel.app/'
+# ]
 CSRF_TRUSTED_ORIGINS = ['https://habitise.vercel.app/']
 
 # Database

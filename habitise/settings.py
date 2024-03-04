98b798bcc621790b011c962eda200864a5a3292a
caioluciofc <caioluciofc@gmail.com>
2024-03-04 10:32:55 -0300

    Cors

--- habitise/settings.py
+++ habitise/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'habitise.wsgi.application'
 AUTH_USER_MODEL = "habitise.User"
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 'https://habitise.vercel.app/'
 ]
